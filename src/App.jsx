9262ad7450a1a975bb4ef4afbff786a9f8352644
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-16 10:40:45 -0300

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './global.css';
+import styles  from './App.module.css';
+
 import { Header } from "./components/Header";
+import { Siderbar } from './components/Sidebar';
 
 function App() {
 
   return (
     <div>
       <Header/>
+
+      <div className={styles.wrapper}>
+        <Siderbar/>
+        <main></main>
+      </div>
     </div>
   )
 }
