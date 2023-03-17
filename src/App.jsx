8ad9920a390e91fab4e45d7f47583dc6183652de
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-17 11:38:30 -0300

    Post content and date

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,43 @@ import { Header } from "./components/Header";
 import { Siderbar } from './components/Sidebar';
 import { Post } from './components/Post';
 
-import './global.css';
 import styles  from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', 
+        content: 'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-03-17 10:00:00'), 
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋'},
+      { type: 'paragraph', 
+        content: 'Acabei de subir mais um projeto no meu portfólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-03-16 10:00:00'),
+  },
+];
 
 function App() {
 
@@ -14,8 +49,15 @@ function App() {
       <div className={styles.wrapper}>
         <Siderbar/>
         <main>
-          <Post/>
-          <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
