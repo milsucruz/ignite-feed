6156cfcd93b7d71de59068f5ce89bdd27f1d69d4
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-21 17:30:49 -0300

    New Comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,10 @@ import styles from './Post.module.css'
 
 export function Post ({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
+    'massa'
   ]);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -25,7 +26,12 @@ export function Post ({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault();
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
  
   return(
@@ -59,7 +65,12 @@ export function Post ({ author, publishedAt, content }) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu Feedback</strong>
 
-        <textarea placeholder='Deixe um comentário' />
+        <textarea
+          name='comment'
+          placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type='submit'>Comentar</button>
@@ -68,7 +79,7 @@ export function Post ({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment/>
+          return <Comment content={comment} />
         })}
       </div>
 
