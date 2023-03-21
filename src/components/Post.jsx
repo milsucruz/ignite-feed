755be71baaf7f9d4b12ab94f760cde5c23731d89
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-21 16:57:01 -0300

    Comments useState

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
-import { format, formatDistanceToNow } from 'date-fns';
+import { format, formatDistanceToNow, set } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
@@ -7,6 +8,10 @@ import { Comment } from './Comment';
 import styles from './Post.module.css'
 
 export function Post ({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    1,
+    2,
+  ]);
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -17,6 +22,12 @@ export function Post ({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, comments.length + 1]);
+  }
+ 
   return(
     <article className={styles.post}>
       <header>
@@ -40,12 +51,12 @@ export function Post ({ author, publishedAt, content }) {
           if(line.type === 'paragraph') {
             return <p>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p> <a href="">{line.content}</a> </p>
+            return <p> <a href="#">{line.content}</a> </p>
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu Feedback</strong>
 
         <textarea placeholder='Deixe um comentário' />
@@ -56,9 +67,9 @@ export function Post ({ author, publishedAt, content }) {
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        {comments.map(comment => {
+          return <Comment/>
+        })}
       </div>
 
     </article>
