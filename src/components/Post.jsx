53b5906d816d05593cae98a974ff2ca62fe9ec00
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-03-22 14:34:08 -0300

    form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post ({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("")
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Este campo é obrigatório!")
   }
 
   function deleteComment(commentToDelete) {
@@ -43,7 +48,9 @@ export function Post ({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeleteOne);
   }
- 
+
+  const isNewCommentEmpty = newCommentText.length == 0;
+
   return(
     <article className={styles.post}>
       <header>
@@ -80,10 +87,14 @@ export function Post ({ author, publishedAt, content }) {
           placeholder='Deixe um comentário'
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Comentar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>
+            Comentar
+          </button>
         </footer>
       </form>
 
